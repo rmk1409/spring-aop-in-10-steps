b507d09d765f611cd60256aa3c063d6d297194a5
Pogorelov Roman <roman.pogorelov@axamit.com>
2019-12-05 16:35:20 +0300

    Initial commit

---
+++ src/main/java/com/veselov/alex/springaopin10steps/SpringAopIn10StepsApplication.java
@@ -0,0 +1,13 @@
+package com.veselov.alex.springaopin10steps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAopIn10StepsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAopIn10StepsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/veselov/alex/springaopin10steps/SpringAopIn10StepsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.veselov.alex.springaopin10steps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAopIn10StepsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
